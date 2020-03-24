97f31c43a90d1ab034492d8d11eabd0278c2c152
nguyenvietanh <vanhng23@gmail.com>
2020-03-24 21:59:44 +0700

    modified add comment line #

--- DemoStudy.rb
+++ DemoStudy.rb
@@ -6,9 +6,9 @@ require './salary.rb'
 class Employee < Salary
 	attr_accessor :id, :date, :time
 
-	def initialize (id, date, time)
-		@id = id
-		@date = date
-		@time = time
+	# def initialize (id, date, time)
+	# 	@id = id
+	# 	@date = date
+	# 	@time = time
 	end
 end  

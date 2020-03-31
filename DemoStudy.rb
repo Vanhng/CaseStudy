b3ca110d7d0599ecaf3a15003a817a102262d5d6
nguyenvietanh <vanhng23@gmail.com>
2020-03-31 15:20:04 +0700

    modify Casestudy

--- DemoStudy.rb
+++ DemoStudy.rb
@@ -1,15 +1,1 @@
-require 'csv'
-require 'time'
-require 'date'
-require './salary.rb'
-
-class Employee < Salary
-	attr_accessor :id, :date, :time
-
-	# def initialize (id, date, time)
-	# 	@id = id
-	# 	@date = date
-	# 	@time = time
-	end
-end  
-abcabcbac
+ábfjashjkahsjkhaksjhsakjhaskhskj
